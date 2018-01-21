19d7b9831a15e3c91e22b388869b4df0489be092
mchang16 <mchang0816@gmail.com>
2018-01-21 03:12:30 -0500

    First commit

---
+++ Secret Project Secret/Assets/script.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+
+	public GameObject door;
+	public GUI text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(0)) {
+			OnMouseDown ();
+		}
+
+	}
+
+	void OnMouseDown(){
+		// this object was clicked - do something
+		Destroy (this.gameObject);
+	}   
+}
